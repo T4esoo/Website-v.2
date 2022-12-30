605383a997a95c37ee8a024c73f1c0921d13c009
Taesoo <104331725+T4esoo@users.noreply.github.com>
2022-12-30 14:02:33 -0800

    Webp test

--- js/main.js
+++ js/main.js
@@ -78,7 +78,3 @@
 })(jQuery);
 var myModal = document.getElementById('myModal')
 var myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
